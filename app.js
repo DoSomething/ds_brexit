3a26c0bc6c4a33c6bfff8457ba58a6f84344284c
deadlybutter <thedeadlybutter@gmail.com>
2016-06-23 09:52:44 -0400

    First pass at a design

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 app.use(express.static(__dirname + '/node_modules/@dosomething/forge/dist'));
+app.use(express.static(__dirname + '/node_modules/@dosomething/forge/assets'));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
